f8c99bbf83657a8c66208317128bcb6093013f9b
majicmoo <megan@theparkes.me.uk>
2014-11-09 23:33:26 +0000

    adding updated sh

--- runjumble.sh
+++ runjumble.sh
@@ -1,6 +1,31 @@
-javac -cp Documents/MutationProject/jumblejars/*:Documents/WAW-Assessment4/Game/lib/jars/*:Documents/WAW-Assessment4/Game/src:Documents/WAW-Assessment4/Game/lib/natives:. -d Documents/WAW-Assessment4/Game/src Documents/WAW-Assessment4/Game/src/stateContainer/Game.java 
+#Compile on Linux
+#javac -cp Documents/MutationProject/jumblejars/*:Documents/WAW-Assessment4/Game/lib/jars/*:Documents/WAW-Assessment4/Game/src:Documents/WAW-Assessment4/Game/lib/natives:. -d Documents/WAW-Assessment4/Game/src Documents/WAW-Assessment4/Game/src/stateContainer/Game.java 
 
+#Run Game on Linux
 #java -Djava.library.path="Documents/WAW-Assessment4/Game/lib/natives" -cp .:Documents/WAW-Assessment4/Game/lib/jars/*:Documents/WAW-Assessment4/Game/src stateContainer.Game
 
+#Mutation test on Linux
+#java -jar Documents/MutationProject/jumblejars/* --classpath=. "Documents/WAW-Assessment4/Game/lib/jars/*:Documents/WAW-Assessment4/Game/src:Documents/WAW-Assessment4/Game/lib/natives/*:."
 
-java -jar Documents/MutationProject/jumblejars/* --classpath=. "Documents/WAW-Assessment4/Game/lib/jars/*:Documents/WAW-Assessment4/Game/src:Documents/WAW-Assessment4/Game/lib/natives/*:."
+#Run Game on Windows
+#java -Djava.library.path="Documents/WAW-Assessment4/Game/lib/natives" -cp .:Documents/WAW-Assessment4/Game/lib/jars/*:Documents/WAW-Assessment4/Game/bin:Documents/WAW-Assessment4/Game/src stateContainer.Game
+
+#Compile tempProject on Windows
+echo "compiling main"
+javac -cp .:/c/Users/Megan/workspace/testProject/jar/junit.jar:/c/Users/Megan/workspace/testProject/src:/c/Users/Megan/Documents/MutationProject/jumblejars/* -sourcepath /c/Users/Megan/workspace/testProject/src /c/Users/Megan/workspace/testProject/src/Temp.java
+
+echo "compiling tests"
+javac -cp .:/c/Users/Megan/workspace/testProject/jar/junit.jar:/c/Users/Megan/workspace/testProject/src:/c/Users/Megan/Documents/MutationProject/jumblejars/*  -sourcepath /c/Users/Megan/workspace/testProject/src/ /c/Users/Megan/workspace/testProject/src/TempTest.java  -verbose
+
+echo " "
+#Run tempProject on Windows -Linux cmd
+echo "running main"
+java -cp /c/Users/Megan/workspace/testProject src.Temp
+
+#echo "running tests"
+#java -cp ./workspace/testProject/src tempProjectTest.Temp_Test
+
+#Mutation test on Windows
+echo " "
+echo "jumble"
+java -jar /c/Users/Megan/Documents/MutationProject/jumblejars/* --classpath=. src/Temp
--- runpit.sh
+++ runpit.sh
@@ -1,6 +1,27 @@
-java -cp Documents/pitjars/*:Documents/WAW-Assessment4/Game/lib/jars/*:. \
+#java -cp /c/Users/Megan/Documents/MutationProject/pitjars/*:/c/Users/Megan/workspace/testProject/jar/junit.jar:/c/Users/Megan/workspace/testProject/src:. \
+#org.pitest.mutationtest.commandline.MutationCoverageReport \
+#--reportDir /c/Users/Megan/Documents/MutationProject/mutationreports \
+#--sourceDirs /c/Users/Megan/workspace/testProject/src \
+#--targetClasses Temp \
+#--targetTests TempTest
+
+
+#run waw
+
+echo "compile waw"
+javac -cp /c/Users/Megan/Documents/MutationProject/jumblejars/*:/c/Users/Megan/Documents/WAW-Assessment4/Game/lib/jars/*:/c/Users/Megan/Documents/WAW-Assessment4/Game/src:/c/Users/Megan/Documents/WAW-Assessment4/Game/lib/natives:. -d /c/Users/Megan/Documents/WAW-Assessment4/Game/src /c/Users/Megan/Documents/WAW-Assessment4/Game/src/stateContainer/Game.java
+
+echo "compile tests"
+javac -cp /c/Users/Megan/Documents/MutationProject/jumblejars/*:/c/Users/Megan/Documents/WAW-Assessment4/Game/lib/jars/*:/c/Users/Megan/Documents/WAW-Assessment4/Game/src:/c/Users/Megan/Documents/WAW-Assessment4/Game/lib/natives:. -d /c/Users/Megan/Documents/WAW-Assessment4/Game/src /c/Users/Megan/Documents/WAW-Assessment4/Game/src/unitTests/*.java
+
+#echo "run waw"
+#java -Djava.library.path="/c/Users/Megan/Documents/WAW-Assessment4/Game/lib/natives" -cp /c/Users/Megan/Documents/MutationProject/jumblejars/*:/c/Users/Megan/Documents/WAW-Assessment4/Game/lib/jars/*:/c/Users/Megan/Documents/WAW-Assessment4/Game/src:/c/Users/Megan/Documents/WAW-Assessment4/Game/lib/natives:. stateContainer.Game
+
+
+echo "run pit"
+java -Djava.library.path="/c/Users/Megan/Documents/WAW-Assessment4/Game/lib/natives" -cp /c/Users/Megan/Documents/MutationProject/jumblejars/*:/c/Users/Megan/Documents/WAW-Assessment4/Game/lib/jars/*:/c/Users/Megan/Documents/WAW-Assessment4/Game/src:/c/Users/Megan/Documents/WAW-Assessment4/Game/lib/natives:/c/Users/Megan/workspace/testProject/jar/junit.jar:. \  
 org.pitest.mutationtest.commandline.MutationCoverageReport \
---reportDir Documents/mutationreports \
+--reportDir /c/Users/Megan/Documents/MutationProject/mutationreports \
+--sourceDirs /c/Users/Megan/Documents/WAW-Assessment4/Game/src \
 --targetClasses competitve.*, coop.*, events.*, logicClasses.*, res.*, stateContainer.*, states.*, util.* \
 --targetTests unitTests.*
---sourceDirs Documents/WAW-Assessment4/Game/src/
