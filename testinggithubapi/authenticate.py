f6de2c6687412516877f8fe8b4c92e9620446843
majicmoo <megan@theparkes.me.uk>
2015-01-10 16:39:55 +0000

    tried to add authentication, only works in bash/terminal

---
+++ testinggithubapi/authenticate.py
@@ -0,0 +1,41 @@
+GITHUB_API = 'https://api.github.com'
+
+from requests import *
+import urllib2
+from urllib2 import *
+import json
+from StringIO import *
+from project import Project
+from runMutationTools import RunMutationTools
+import time
+from retrying import retry
+import requests
+import getpass
+from urlparse import urljoin
+
+class Authenticate:
+    def __init__(self):
+        pass
+
+    def doAuthenticate(self):
+        username = raw_input('Github username: ')
+        print username
+        password = getpass.getpass("Github password: ")
+        note = raw_input('Note (optional): ')
+        url = urljoin(GITHUB_API, ' authorizations')
+        print url
+        payload = {}
+        if note:
+            payload['note']=note
+        res = requests.post(url, auth = (username, password), data=json.dumps(payload))
+
+        j = json.loads(res.text)
+        if res.status_code >= 400:
+            msg = j.get('message', ' UNDEFINED ERROR (no error description from server)')
+            print 'ERROR: %s' % msg
+            return
+        token=j['token']
+        print 'New token: %s' %token
+
+test = Authenticate()
+test.doAuthenticate()
